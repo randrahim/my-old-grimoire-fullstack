070b8b26fbe79f72ad48b8124e8fc037163d6b0a
randrahim <rrahim3@gatech.edu>
2024-12-31 00:41:08 -0500

    Adding the userId to the bookSchema

--- backend/models/book.js
+++ backend/models/book.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const bookSchema = mongoose.Schema({
+  userId: { type: String, required: true },
   title: { type: String, required: true },
   author: { type: String, required: true },
   imageUrl: { type: String, required: true },
@@ -12,7 +13,7 @@ const bookSchema = mongoose.Schema({
       grade: { type: Number, required: true, min: 0, max: 5 },
     },
   ],
-  averageRating: { type: Number, required: true },
+  averageRating: { type: Number, default: 0 },
 });
 
 module.exports = mongoose.model("Book", bookSchema);
