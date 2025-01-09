a6185d39dcb46c3b5d903a80d59eadca42c83468
randrahim <rrahim3@gatech.edu>
2025-01-08 20:22:41 -0500

    updating the deleteBook function in books.js in controllers

--- backend/controllers/books.js
+++ backend/controllers/books.js
@@ -62,20 +62,6 @@ exports.modifyBook = (req, res, next) => {
     });
 };
 
-exports.deleteBook = (req, res, next) => {
-  Book.deleteOne({ _id: req.params.id })
-    .then(() => {
-      res.status(200).json({
-        message: "Deleted!",
-      });
-    })
-    .catch((error) => {
-      res.status(400).json({
-        error: error,
-      });
-    });
-};
-
 exports.getAllBooks = (req, res, next) => {
   Book.find()
     .then((books) => {
@@ -86,4 +72,44 @@ exports.getAllBooks = (req, res, next) => {
         error: error,
       });
     });
+};
+
+// exports.deleteBook = (req, res, next) => {
+//   Book.deleteOne({ _id: req.params.id })
+//     .then(() => {
+//       res.status(200).json({
+//         message: "Deleted!",
+//       });
+//     })
+//     .catch((error) => {
+//       res.status(400).json({
+//         error: error,
+//       });
+//     });
+// };
+
+exports.deleteBook = (req, res, next) => {
+  Book.findOne({ _id: req.params.id }).then((book) => {
+    if (!book) {
+      return res.status(404).json({
+        error: new Error("Object not found!"),
+      });
+    }
+    if (book.userId !== req.auth.userId) {
+      return res.status(401).json({
+        error: new Error("Request not authorized!"),
+      });
+    }
+    Book.deleteOne({ _id: req.params.id })
+      .then(() => {
+        res.status(200).json({
+          message: "Deleted!",
+        });
+      })
+      .catch((error) => {
+        res.status(400).json({
+          error: error,
+        });
+      });
+  });
 };
