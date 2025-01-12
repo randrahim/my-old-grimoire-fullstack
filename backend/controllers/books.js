28671bf476bc1e51eb0b1126d1fb378959df7d9c
randrahim <rrahim3@gatech.edu>
2025-01-11 22:38:10 -0500

    Modify the POST & Put Route

--- backend/controllers/books.js
+++ backend/controllers/books.js
@@ -1,15 +1,17 @@
 const Book = require("../models/book");
 
 exports.createBook = (req, res, next) => {
+  req.body.book = JSON.parse(req.body.book);
+  const url = req.protocol + "://" + req.get("host");
   const book = new Book({
-    title: req.body.title,
-    author: req.body.author,
-    imageUrl: req.body.imageUrl,
-    year: req.body.year,
-    genre: req.body.genre,
-    ratings: req.body.ratings,
-    averageRating: req.body.averageRating,
-    userId: req.body.userId,
+    title: req.body.book.title,
+    author: req.body.book.author,
+    imageUrl: req.body.book.imageUrl,
+    year: req.body.book.year,
+    genre: req.body.book.genre,
+    ratings: req.body.book.ratings,
+    averageRating: req.body.book.averageRating,
+    userId: req.body.book.userId,
   });
   book
     .save()
@@ -40,17 +42,45 @@ exports.getOneBook = (req, res, next) => {
 };
 
 exports.modifyBook = (req, res, next) => {
-  const book = new Book({
-    _id: req.params.id,
-    title: req.body.title,
-    author: req.body.author,
-    imageUrl: req.body.imageUrl,
-    year: req.body.year,
-    genre: req.body.genre,
-    ratings: req.body.ratings,
-    averageRating: req.body.averageRating,
-    userId: req.body.userId,
-  });
+  let book = new Book({ _id: req.params._id });
+  if (req.file) {
+    const url = req.protocol + "://" + req.get("host");
+    req.body.book = JSON.parse(req.body.book);
+    book = {
+      _id: req.params.id,
+      title: req.body.book.title,
+      author: req.body.book.author,
+      imageUrl: url + "/images/" + req.file.filename,
+      year: req.body.book.year,
+      genre: req.body.book.genre,
+      ratings: req.body.book.ratings,
+      averageRating: req.body.book.averageRating,
+      userId: req.body.book.userId,
+    };
+  } else {
+    book = {
+      _id: req.params.id,
+      title: req.body.title,
+      author: req.body.author,
+      imageUrl: req.body.imageUrl,
+      year: req.body.year,
+      genre: req.body.genre,
+      ratings: req.body.ratings,
+      averageRating: req.body.averageRating,
+      userId: req.body.userId,
+    };
+  }
+  Book.updateOne({ _id: req.params.id }, book)
+    .then(() => {
+      res.status(201).json({
+        message: "Book updated successfully!",
+      });
+    })
+    .catch((error) => {
+      res.status(400).json({
+        error: error,
+      });
+    });
   Book.updateOne({ _id: req.params.id }, book)
     .then(() => {
       res.status(201).json({
