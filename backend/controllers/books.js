14a2a962bf4b16912c11ea8bdf9d294af8ca1069
randrahim <rrahim3@gatech.edu>
2025-01-11 22:40:48 -0500

    Modify the Delete Route

--- backend/controllers/books.js
+++ backend/controllers/books.js
@@ -1,4 +1,5 @@
 const Book = require("../models/book");
+const fs = require("fs");
 
 exports.createBook = (req, res, next) => {
   req.body.book = JSON.parse(req.body.book);
@@ -122,26 +123,19 @@ exports.getAllBooks = (req, res, next) => {
 
 exports.deleteBook = (req, res, next) => {
   Book.findOne({ _id: req.params.id }).then((book) => {
-    if (!book) {
-      return res.status(404).json({
-        error: new Error("Object not found!"),
-      });
-    }
-    if (book.userId !== req.auth.userId) {
-      return res.status(401).json({
-        error: new Error("Request not authorized!"),
-      });
-    }
-    Book.deleteOne({ _id: req.params.id })
-      .then(() => {
-        res.status(200).json({
-          message: "Deleted!",
+    const filename = book.imageUrl.split("/images/")[1];
+    fs.unlink("images/" + filename, () => {
+      Book.deleteOne({ _id: req.params.id })
+        .then(() => {
+          res.status(200).json({
+            message: "Deleted!",
+          });
+        })
+        .catch((error) => {
+          res.status(400).json({
+            error: error,
+          });
         });
-      })
-      .catch((error) => {
-        res.status(400).json({
-          error: error,
-        });
-      });
+    });
   });
 };
